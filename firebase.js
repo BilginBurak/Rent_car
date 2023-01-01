d67249e9538f1ca4437245e555196ba54a79a2ae
BilginBurak <106628063+BilginBurak@users.noreply.github.com>
2023-01-01 15:26:20 +0300

    new year first load

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD8huoUJahSSgfeVrduetfga8VcAn3ohUA",
+  authDomain: "rent-car-b7bab.firebaseapp.com",
+  projectId: "rent-car-b7bab",
+  storageBucket: "rent-car-b7bab.appspot.com",
+  messagingSenderId: "307208189325",
+  appId: "1:307208189325:web:49734a018255314d7039c4"
+};
+  
+firebase.initializeApp(firebaseConfig);
